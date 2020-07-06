c61b7bcac52adb3820dce2e7547e32bbd17e1270
Ingo Bernhardt <bernhardt@exzentriker.it>
2020-07-06 20:46:19 +0200

    Annotation Reader

--- Kofus/Deepl/src/Kofus/Deepl/Service/DeeplService.php
+++ Kofus/Deepl/src/Kofus/Deepl/Service/DeeplService.php
@@ -1,13 +1,12 @@
 <?php
 namespace Kofus\Deepl\Service;
 use Kofus\System\Service\AbstractService;
-use Zend\Uri\UriFactory;
-use Zend\Http\Client as HttpClient;
-use Kofus\GoogleMaps\Geocode;
+use Kofus\System\Node\NodeInterface;
+
 
 class DeeplService extends AbstractService
 {
-    public function translate($txt, $params=array())
+    public function translateText($txt, $params=array())
     {
         $uri = $this->config()->get('deepl.uri', 'https://api.deepl.com/v2/translate');
         
@@ -35,5 +34,32 @@ class DeeplService extends AbstractService
         }
         
         return $cache->getItem($cacheId);
+    }
+    
+    public function translateEntity(NodeInterface $entity)
+    {
+        $translationService = $this->getServiceLocator()->get('KofusTranslationService');
+        $locales = $this->config()->get('locales.enabled');
+        $builder = new \Kofus\Deepl\Annotation\AnnotationBuilder();
+        $spec = $builder->getFormSpecification($entity);
+        $arraySpec = \Zend\Stdlib\ArrayUtils::iteratorToArray($spec);
+        $defaultLocale = $this->config()->get('locales.default');
+        $deeplOptions = array('source_lang' => strtoupper(substr($defaultLocale, 0, 2)));
+        
+        foreach ($arraySpec['elements'] as $delta) {
+            if (isset($delta['spec']['type']) && 'auto-translate' == $delta['spec']['type']) {
+                $deeplOptions = array();
+                if (isset($delta['spec']['options'])) $deeplOptions = $delta['spec']['options'];
+                $attrib = $delta['spec']['name'];
+                $method = 'get' . ucfirst($attrib);
+                foreach ($locales as $locale) {
+                    $value = $entity->$method();
+                    
+                    $deeplOptions['target_lang'] = strtoupper(substr($locale, 0, 2));
+                    $tValue = $this->translateText($value, $deeplOptions);
+                    $translationService->addNodeTranslation($entity, $method, $tValue, $locale);
+                }
+            }
+        }
     }
 }
